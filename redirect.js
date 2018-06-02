a31987867b9f594e0358281860fdcf9fa3d6d7d5
Sandeep Sukumaran <sandeepsukumaran@hotmail.com>
2018-06-02 17:17:48 -0500

    Add files via upload

---
+++ redirect.js
@@ -0,0 +1,13 @@
+var targetURL = "https://old.reddit.com"
+
+function redirect(requestDetails){
+	return{
+		redirectURL: targetURL+requestDetails.url.match(/https?:\/\/[^\/]+([\S\s]*)/)[1]
+	}
+}
+
+browser.webRequest.onBeforeRequest.addListener(
+	redirect,
+	{urls:["*://reddit.com/*","*://www.reddit.com/*","*://np.reddit.com/*","*://new.reddit.com/*"],
+		types:["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]},
+	['blocking']);
